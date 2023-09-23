1c2955fd996a0e58d471333a0e3f5ce047aeb24c
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-09-23 10:33:53 +0900

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+// run `node index.js` in the terminal
+
+import { Hono } from 'hono';
+
+const middleware = () => {
+  return async (c, next) => {
+    // read .text() in middleware
+    const body = await c.req.text();
+    console.log('middleware', body);
+    await next();
+  };
+};
+
+const app = new Hono();
+
+app.post('/', middleware(), async (c) => {
+  // read .json()
+  const body = await c.req.json();
+  console.log('handler', body);
+  return c.text('ok');
+});
+
+app.onError((e, c) => {
+  console.error(e);
+  return c.text(e.message, 500);
+});
+
+app.request('/', {
+  method: 'POST',
+  body: JSON.stringify({ foo: 'bar' }),
+});
